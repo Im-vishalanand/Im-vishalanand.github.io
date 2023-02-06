c550f6a8cf7cac90adbf7b5d4e4076516cfb93c4
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-02-07 02:30:37 +0530

    navbar done with responsiveness addition

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let menu=document.getElementById("menu-icon");
+let navbar=document.getElementById("navbar");
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+});
+
