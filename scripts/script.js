693b0de3abedbc3e3eff3fcbeac3c5c09780ae24
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-02-11 03:38:20 +0530

    about done

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,23 @@
 let menu=document.getElementById("menu-icon");
 let navbar=document.getElementById("navbar");
-
+let m=document.querySelector(".main-body")
+// let count=0;
 menu.addEventListener('click',()=>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+    
+    // m.style.marginTop="300px";
+    // count++;
+    // if(count%2==0){
+    //     // m.style.marginTop="0px";
+
+    // }
+    if(m.classList.contains("m-t")){
+        m.classList.remove("m-t");
+    }
+    else{
+        m.classList.add("m-t");
+    }
 });
 
 
